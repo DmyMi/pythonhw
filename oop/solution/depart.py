aacc81fbb3f88054ca826dca7ca3cf191dbc8a8f
nitroboy <nitroboy@duty-linux1.zeo.lcl>
2018-11-08 07:34:17 +0200

    test

--- oop/solution/depart.py
+++ oop/solution/depart.py
@@ -1,69 +1,89 @@
 class Departament(object):
-	def __init__(self, managers):
-		self.managers = []
+
+	def __init__(self, managers=[]):
+		self.managers = managers
 
 	def pay_salary(self):
-		GOT_SALARY = "{name} {sname}: got salary: {salary}"
-		for manager in managers:
-			salary = manager.salary
-			print(GOT_SALARY.format(manager.name, manager.sname, salary)
+		for manager in self.managers:
+			manager.give_salary()
+			for employee in manager.team:
+				employee.give_salary()
 
-	@staticmethod
-	def pay_salary_employeers(employees):
-		for emp in employees:
-			if (type(emp) == Developer)
-				salary = emp.salary()
-				print(GOT_SALARY.format(manager.name, manager.sname, salary)
-			elif (type(emp) == Designer)
-				print(GOT_SALARY.
+	def hire_manager(self, name, sname, salary, exp, team=None):
+		new_manager = Manager(name, sname, salary, exp, [] if not team else team)
+		self.managers.append(new_manager)
+
 
 class Employee(object):
+
 	def __init__(self, name, sname, salary, exp):
 		self.name = name
 		self.sname = sname
-		self.salary = salary
+		self.base_salary = salary
 		self.exp = exp
 
-	def got_salary(self)
-		salary = self.salary
-		if exp > 5:
+	def calculate_salary(self):
+		salary = self.base_salary
+		if self.exp > 5:
 			salary = salary * 1.2 + 500
-		elif exp > 2:
+		elif self.exp > 2:
 			salary += 200
 		return salary
+
+	def _print_salary_msg(self, salary):
+		GOT_SALARY = "{0} {1}: got salary: {2}"
+		print(GOT_SALARY.format(self.name, self.sname, salary))
+
+	def give_salary(self):
+		salary = self.calculate_salary()
+		self._print_salary_msg(salary)
+
 
 
 class Manager(Employee):
-	def __init__(self, name, sname, salary, exp, team=None: list):
-		self.team = team
-		super().__init__(name, sname, salary, exp, None)
 
-	def got_salary(self):
-		salary = super().get_salary() * self.coeff
+	def __init__(self, name, sname, salary, exp, team=None):
+		self.team = [] if not team else team
+		super().__init__(name, sname, salary, exp)
+
+	def hire_employee(self, employee: Employee):
+		self.team.append(employee)
+
+	def calculate_salary(self):
+		salary = super().calculate_salary()
 		devs = 0
-		for emp in manager.team:
+		for emp in self.team:
 			if type(emp) == Developer:
 				devs += 1
-		if not team:
+		if not self.team:
 			pass		
-		elif devs / len(manager.team) > 0.5:
+		elif devs / len(self.team) > 0.5:
 			salary *= 1.1
-		elif len(manager.team) > 5:
+		elif len(self.team) > 5:
 			salary += 200
-		elif len(manager.team) > 10:
+		elif len(self.team) > 10:
 			salary += 300
 		return salary
 
+	def give_salary(self):
+		self._print_salary_msg(self.calculate_salary())
+
 
 class Developer(Employee):
+
 	def __init__(self, name, sname, salary, exp):
 		super().__init__(name, sname, salary, exp)
 
 
-class Designer(Employee)
-	def __init__(self, name, sname, salary, exp, coeff)
+class Designer(Employee):
+
+	def __init__(self, name, sname, salary, exp, coeff):
 		self.coeff = coeff
 		super().__init__(name, sname, salary, exp)
 	
-	def got_salary(self)
-		return super().get_salary() * self.coeff
+	def calculate_salary(self):
+		salary = super().calculate_salary() * self.coeff
+		return salary
+
+	def give_salary(self):
+		self._print_salary_msg(self.calculate_salary())
--- oop/solution/tests.py
+++ oop/solution/tests.py
@@ -1,1 +1,34 @@
-import unittests
+import unittest as ut
+
+from depart import Departament, Developer, Designer
+
+
+class TestSalaries(ut.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		cls.Depart = Departament()
+		cls.Depart.hire_manager('Vasil', 'Boss', 777, 3)
+		devs = [("Junior", "Dev-H@cker", 1500, 1), ("Middle", "Dev-H@cker", 3000, 3), ("Senior", "Dev-H@cker", 7000, 10)]
+		for dev in devs:
+			_dev = Developer(*dev)
+			cls.Depart.managers[0].hire_employee(_dev)
+		designers = (("Junior", "Designer", 777, 1, 1), ("Middle", "Designer", 1700, 3, 1.1), ("Da", "Vinci", 7000, 10, 10))
+		for designer in designers:
+			_designer = Designer(*designer)
+			cls.Depart.managers[0].hire_employee(_designer)
+		cls.Depart.hire_manager('Dima', 'Boss2', 1000, 7, [])
+
+	def test_count_of_managers(self):
+		managers_count = 0
+		for x in self.Depart.managers:
+			managers_count += 1
+		self.assertEqual(2, managers_count)
+
+	def test_salaries(self):
+		self.Depart.pay_salary()
+
+	#def test_B(self):
+	#	print("Hello")
+
+ut.main()
